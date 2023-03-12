6a53f2b61ba6ed7b4e6f2cba8b99696dc77dd6da
Josh S <joshsevero208@gmail.com>
2023-03-11 20:50:22 -0500

    Attempted Patch for previous Commit
    Github doesn't like me :(

---
+++ src/Pages/OCHS.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Assets/CSS/OCHS.min.css'
+
+function OCHS() {
+    return (
+        <section id="section-1">
+            <video src="https://cdn.randomassfights.live/Videos/North%20America/Pennsylvania/Oil%20City%20High%20School/S9dhYH.mp4" controls='controls' width='250' height='400'></video>
+        </section>
+    );
+}
+
+export default OCHS;
