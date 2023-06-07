2bc3038eb184e61216f7de6f43f9d872d4835e53
DIVISIONSolar <joshsevero208@gmail.com>
2023-06-07 14:42:26 -0400

    fixed bug w/ navbar (Dropdown would still show even after going to another page)

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import '../Assets/CSS/Navbar.min.css';
 
 function Navbar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
-  const handleDropdownToggle = () => {
+  const handleDropdownToggle = (event) => {
+    event.stopPropagation();
     setIsDropdownOpen(!isDropdownOpen);
   };
 
   const handleOptionClick = () => {
     setIsDropdownOpen(false);
   };
+
+  const handleClickOutsideDropdown = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsDropdownOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', handleClickOutsideDropdown);
+
+    return () => {
+      window.removeEventListener('click', handleClickOutsideDropdown);
+    };
+  }, []);
 
   return (
     <nav className="bg-nav-black p-[17px] text-center font-bold text-white font-abril-fatface font-rubik">
@@ -26,7 +42,10 @@ function Navbar() {
           Schools
         </button>
         {isDropdownOpen && (
-          <div className="absolute left-0 mt-2 w-40 bg-white shadow-lg rounded-md">
+          <div
+            ref={dropdownRef}
+            className="absolute left-0 mt-2 w-40 bg-white shadow-lg rounded-md"
+          >
             <Link
               to="/Schools/Districts/FHS"
               className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
