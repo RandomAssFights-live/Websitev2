e19ec1bf6d8e7b5779f3779d5ec0c605a4bb415a
Josh S <me@joshsevero.dev>
2023-06-08 02:33:55 -0400

    Add new video

--- src/Pages/Random.jsx
+++ src/Pages/Random.jsx
@@ -9,6 +9,7 @@ function Random() {
                     <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
                         <video className='h-[23rem]' src="https://cdn.randomassfights.live/Videos/Random/VxALlH.mp4" controls='controls' width='250' height='400'></video>
                         <video className='h-[23rem]' src="https://cdn.randomassfights.live/Videos/Random/XOfKzH.mp4" controls='controls' width='250' height='400'></video>
+                        <video className='h-[23rem]' src="https://cdn.randomassfights.live/Videos/Random/DjfgTz.mp4" controls='controls' width='250' height='400'></video>
                     </div>
                 </div>
             </div>
