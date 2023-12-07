6fd21188200a43976c08fc8f94d081c2727b0754
Josh S <me@joshsevero.dev>
2023-12-07 17:19:36 -0500

    update date and add videos

--- src/Pages/Random.jsx
+++ src/Pages/Random.jsx
@@ -4,7 +4,7 @@ import CountdownTimer from "../Components/Timer";
 import "../Assets/CSS/input.css";
 
 function Random() {
-  const targetDate = new Date("2023-12-05 13:31:37");
+  const targetDate = new Date("2023-12-07 22:17:22");
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -32,11 +32,11 @@ function Random() {
     "https://cdn.randomassfights.live/Videos/Random/HyF4Rz.mp4",
     "https://cdn.randomassfights.live/Videos/Random/LkO6Hz.mp4",
     "https://cdn.randomassfights.live/Videos/Random/SBX3A15.mp4",
-
-"https://cdn.randomassfights.live/Videos/Random/F9ex00.mp4",
-
-"https://cdn.randomassfights.live/Videos/Random/Vyva9B.mp4"
-
+    "https://cdn.randomassfights.live/Videos/Random/F9ex00.mp4",
+    "https://cdn.randomassfights.live/Videos/Random/Vyva9B.mp4"
+    "https://cdn.randomassfights.live/Videos/Random/46kmDx.mp4",
+    "https://cdn.randomassfights.live/Videos/Random/ga3Qhq.mp4",
+    "https://cdn.randomassfights.live/Videos/Random/ZAGx1h.mp4",
   ];
 
   // Calculate the start and end indices for the videos on the current page
