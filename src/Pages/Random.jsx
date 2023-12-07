c3edd386309b276899a681cdd6aaeae7edbd40c5
Josh S <me@joshsevero.dev>
2023-12-07 17:15:30 -0500

    update date

--- src/Pages/Random.jsx
+++ src/Pages/Random.jsx
@@ -4,7 +4,7 @@ import CountdownTimer from "../Components/Timer";
 import "../Assets/CSS/input.css";
 
 function Random() {
-  const targetDate = new Date("2023-11-11 04:14:00");
+  const targetDate = new Date("2023-12-05 13:31:37");
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
 
